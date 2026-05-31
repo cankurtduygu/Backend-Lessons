e7002c30fe6fe720ae91599ce4d6aad757070712
Duygu cankurt <dyg.cankurt17@gmail.com>
2026-06-01 01:39:51 +0200

    add selfStudy controllers and routers of users

--- Self-Study/rent-a-car/src/index.js
+++ Self-Study/rent-a-car/src/index.js
@@ -1,11 +1,10 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-require("dotenv").config();
+require('dotenv').config();
 const PORT = process.env?.PORT || 8000;
-
 
 /* ------------------------------------------------------- */
 //* Middlewares:
@@ -13,20 +12,56 @@ const PORT = process.env?.PORT || 8000;
 // Accept JSON:
 app.use(express.json());
 
+// Auhentication:
+// app.use(require('./middlewares/authentication'));
 
-app.get("/", (req, res) => {
-    res.send("Rent A Car API")
-})
+// Query Handler
+app.use(require('./middlewares/queryHandler'));
+
+// Logger:
+app.use(require('./middlewares/logger'));
+
+/* ------------------------------------------------------- */
+//* Routes:
+
+// HomePath
+app.all('/', (req, res) => {
+  res.send({
+    error: false,
+    message: 'Welcome to RENT A CAR API',
+    docs: {
+      swagger: '/documents/swagger',
+      redoc: '/documents/redoc',
+      json: '/documents/json',
+    },
+    user: req.user,
+  });
+});
+
+// Other path
+app.use("/", require("./routes/"));
+
+/* ------------------------------------------------------- */
+//* Error Handlers
+
+// Not found route
+app.all("/*splat", (req, res) => {
+  res.errStatusCode = 404;
+  throw new Error("Route is not found.");
+});
+
+// Error handler
+app.use(require("./middlewares/errorHandler"));
 
 
 /* ----------------------------------------- */
 const server = app.listen(PORT, async () => {
-  const { dbConnection } = require("./configs/dbConnection");
+  const { dbConnection } = require('./configs/dbConnection');
 
   await dbConnection()
     .then(() => console.log(`running at: http://127.0.0.1:${PORT}`))
     .catch(() => {
-      console.log("Server shutting down due to DB connection failure.");
+      console.log('Server shutting down due to DB connection failure.');
       server.close(() => process.exit(1));
     });
 });
---
+++ Self-Study/rent-a-car/src/routes/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// ROUTER INDEX:
+
+// URL: /
+
+// auth:
+// router.use("/auth", require("./auth"));
+// user:
+router.use("/users", require("./user"));
+
+
+// document:
+// router.use("/documents", require("./document"));
+
+/* ------------------------------------------------------- */
+module.exports = router;
